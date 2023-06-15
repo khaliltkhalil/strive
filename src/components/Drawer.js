6dcfc70090beb619665bd72f1d71a8e18a6500af
Khalil Khalil <khalil.t.khalil@gmail.com>
2023-06-15 13:00:49 -0500

    install react-router-dom@5

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -30,6 +30,7 @@ function Drawer() {
         <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
         <ul className="menu p-4 w-80 h-full bg-base-200 text-base-content">
           {/* Sidebar content here */}
+
           <li>
             <a>Sidebar Item 1</a>
           </li>
