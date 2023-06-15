f894e49478d39bd439c57082d4008dcd90bd4e9f
Khalil Khalil <khalil.t.khalil@gmail.com>
2023-06-15 12:45:06 -0500

    change hide/show drawer icon

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -8,9 +8,21 @@ function Drawer() {
         {/* Page content here */}
         <label
           htmlFor="my-drawer-2"
-          className="btn btn-primary drawer-button lg:hidden"
+          className="btn btn-square btn-ghost lg:hidden"
         >
-          Open drawer
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            className="inline-block w-5 h-5 stroke-current"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16M4 18h16"
+            ></path>
+          </svg>
         </label>
       </div>
       <div className="drawer-side">
