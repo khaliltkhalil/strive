92a4254359c9dc595fbe59206cbc98103efceb7b
Khalil Khalil <khalil.t.khalil@gmail.com>
2023-06-15 12:52:33 -0500

    move hide/show drawer icon to uper left corner

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -4,26 +4,27 @@ function Drawer() {
   return (
     <div className="drawer lg:drawer-open">
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+      <label
+        htmlFor="my-drawer-2"
+        className="btn btn-square btn-ghost lg:hidden"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          className="inline-block w-5 h-5 stroke-current"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M4 6h16M4 12h16M4 18h16"
+          ></path>
+        </svg>
+      </label>
       <div className="drawer-content flex flex-col items-center justify-center">
         {/* Page content here */}
-        <label
-          htmlFor="my-drawer-2"
-          className="btn btn-square btn-ghost lg:hidden"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            className="inline-block w-5 h-5 stroke-current"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M4 6h16M4 12h16M4 18h16"
-            ></path>
-          </svg>
-        </label>
+        <h1>content</h1>
       </div>
       <div className="drawer-side">
         <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
