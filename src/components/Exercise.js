123cb621ef1672f9dfe418eb5fa5a0c4cc4e8610
Khalil Khalil <khalil.t.khalil@gmail.com>
2023-06-16 14:15:59 -0500

    fix exercise looks when cards are different size

--- src/components/Exercise.js
+++ src/components/Exercise.js
@@ -9,9 +9,13 @@ function Exercise({ exercise }) {
     <div className="card w-96 bg-base-100 shadow-xl">
       <div className="card-body">
         <h2 className="card-title border-b-4">{exercise.name}</h2>
-        <section className="w-1/2 flex flex-col gap-1">{renderedSets}</section>
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now</button>
+        <div className="h-full flex flex-col justify-between">
+          <section className="w-1/2 flex flex-col gap-1">
+            {renderedSets}
+          </section>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Edit</button>
+          </div>
         </div>
       </div>
     </div>
