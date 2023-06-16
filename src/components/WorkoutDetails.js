33305b13163c3444fef4b35163eb338a14c8e618
Khalil Khalil <khalil.t.khalil@gmail.com>
2023-06-16 13:03:57 -0500

    write WorkoutDetails component

--- src/components/WorkoutDetails.js
+++ src/components/WorkoutDetails.js
@@ -1,8 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import Exercise from "./Exercise";
 function WorkoutDetails() {
   const { id } = useParams();
-  return <div>WorkoutDetails</div>;
+  const [exercises, setExercises] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(`http://localhost:3000/exercises/?workoutId=${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setExercises(data);
+        setIsLoading(false);
+      });
+  }, []);
+
+  const renderedExercises = exercises.map((exercise) => (
+    <Exercise key={exercise.id} workout={exercise} />
+  ));
+
+  if (isLoading) {
+    return <div>Loading Workout Details...</div>;
+  }
+
+  return (
+    <div>
+      <div className="flex flex-wrap gap-2 w-full">{renderedExercises}</div>
+    </div>
+  );
 }
 
 export default WorkoutDetails;
