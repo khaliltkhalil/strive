72e09b9812b4634e59dfe085f8a4c3824ae6f756
Khalil Khalil <khalil.t.khalil@gmail.com>
2023-06-16 13:56:03 -0500

    add bottom border to ecersize title

--- src/components/Exercise.js
+++ src/components/Exercise.js
@@ -8,7 +8,7 @@ function Exercise({ exercise }) {
   return (
     <div className="card w-96 bg-base-100 shadow-xl">
       <div className="card-body">
-        <h2 className="card-title">{exercise.name}</h2>
+        <h2 className="card-title border-b-4">{exercise.name}</h2>
         <section className="w-1/2 flex flex-col gap-1">{renderedSets}</section>
         <div className="card-actions justify-end">
           <button className="btn btn-primary">Buy Now</button>
