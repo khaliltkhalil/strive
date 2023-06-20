c67daf95b000637bc825e21f372295d27372017c
Khalil Khalil <khalil.t.khalil@gmail.com>
2023-06-20 12:09:00 -0500

    add Add Set functionality to AddWorkout component

--- src/components/AddWorkout.js
+++ src/components/AddWorkout.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import Exercise from "./Exercise";
 
 function AddWorkout() {
   const [workout, setworkout] = useState({});
@@ -14,6 +15,70 @@ function AddWorkout() {
       ...exercise,
       [e.target.name]: e.target.value,
     });
+  }
+
+  function handleFormSubmit(e) {
+    e.preventDefault();
+    if (!exercise.name) {
+      return;
+    }
+    //check if there is already an exercise for this workout
+    console.log(exercises);
+    const currentExercise = exercises.find(
+      (exerciseItem) => exercise.name === exerciseItem.name
+    );
+    console.log(currentExercise);
+    if (!currentExercise) {
+      // send a post request to create the exersise
+      fetch(`http://localhost:3000/exercises/`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: exercise.name,
+          sets: [
+            {
+              weight: exercise.weight,
+              reps: exercise.reps,
+            },
+          ],
+          workoutId: workout.id,
+        }),
+      })
+        .then((res) => res.json())
+        .then((createdExercise) => {
+          setExercises([createdExercise, ...exercises]);
+        });
+    } else {
+      //  send a patch request to update the exercise
+      fetch(`http://localhost:3000/exercises/${currentExercise.id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          sets: [
+            ...currentExercise.sets,
+            {
+              weight: exercise.weight,
+              reps: exercise.reps,
+            },
+          ],
+        }),
+      })
+        .then((res) => res.json())
+        .then((updatedExercise) => {
+          setExercises(
+            exercises.map((exerciseItem) => {
+              if (exerciseItem.id == currentExercise.id) {
+                return updatedExercise;
+              }
+              return exerciseItem;
+            })
+          );
+        });
+    }
   }
 
   useEffect(() => {
@@ -54,12 +119,15 @@ function AddWorkout() {
       });
   }, []);
 
+  const renderedExercises = exercises.map((exercise) => (
+    <Exercise key={exercise.id} exercise={exercise} />
+  ));
   return (
     <div className="flex flex-col gap-2 items-center">
       <h1 className="text-lg">{workout.date}</h1>
       <div className="card w-96 bg-base-100 shadow-xl">
         <div className="card-body">
-          <form className="flex flex-col gap-2">
+          <form className="flex flex-col gap-2" onSubmit={handleFormSubmit}>
             <div className="form-control w-full max-w-xs gap-4">
               <section>
                 <label className="label">
@@ -108,6 +176,9 @@ function AddWorkout() {
             </div>
           </form>
         </div>
+      </div>
+      <div>
+        <div className="flex flex-wrap gap-2 w-full">{renderedExercises}</div>
       </div>
     </div>
   );
