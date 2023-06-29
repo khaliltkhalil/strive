2e4ffcc43b169710cfd64665be253cf74a89fae2
Khalil Khalil <khalil.t.khalil@gmail.com>
2023-06-29 14:28:15 -0500

    clean warnings

--- src/index.js
+++ src/index.js
@@ -3,13 +3,10 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./components/App";
 import { BrowserRouter as Router } from "react-router-dom";
-import { AppProvider } from "./context/appContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <AppProvider>
-    <Router>
-      <App />
-    </Router>
-  </AppProvider>
+  <Router>
+    <App />
+  </Router>
 );
